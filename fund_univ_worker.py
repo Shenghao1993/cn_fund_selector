bb8395fb5864e4b8f096b33e8581f33e758e4461
Shenghao1993 <wangshenghao1993@gmail.com>
2019-10-01 22:40:28 +0800

    Change the source of the fund universe

--- fund_univ_worker.py
+++ fund_univ_worker.py
@@ -1,55 +1,53 @@
 import os
 import csv
-import yaml
 import argparse
 import logging
 import logging.config
+import yaml
 import requests
-from bs4 import BeautifulSoup
 from addict import Dict as Addict
 
 
 class FundUnivWorker:
     def __init__(self, url, output_fp):
         """Initialization
-        @param url
-        @param output_fp
-
         """
         self.url = url
         self.output_fp = output_fp
-
+    
     def _crawl(self):
-        ret = requests.get(url=self.url)
-        soup = BeautifulSoup(ret.text, features='html.parser')
-        # fund_table
-        div = soup.find(name='div', attrs={'class': 'box'})
-        td_cells = div.find_all(name='td')
-        fund_dict = {}
-        for cell in td_cells:
-            text = cell.get_text()
-            if text:
-                name = text[:-8]
-                index = text[-7:-1]
-                logging.info("name: %s  index: %s", name, index)
-                if index not in fund_dict:
-                    fund_dict[index] = name
-        return fund_dict
+        rsp = requests.get(url=self.url)
+        results = rsp.text.split('],[')
+        logging.info("%s funds found from the source website.", len(results))
+        funds = []
+        for result in results:
+            quote_pos = result.index('"')
+            fund_info = result[quote_pos:]
+            fund_info = fund_info.replace('[', '')
+            fund_info = fund_info.replace(']', '')
+            fund_info = fund_info.replace('"', '')
+            fund_info = fund_info.replace(';', '')
+            funds.append(fund_info.split(','))
+        return funds    
 
     def _output(self, funds):
-        # Remove any eixsitng output file
+        # Remove any exisitng output file
         if os.path.exists(self.output_fp):
             os.remove(self.output_fp)
 
+        counter = 0
         with open(self.output_fp, 'a') as f:
             write = csv.writer(f)
-            for index, name in funds.items():
-                write.writerow([index, name])
-
+            write.writerow(["基金代码", "基金简拼", "基金名称", "基金类型", "基金全拼"])
+            for fund in funds:
+                write.writerow(fund)
+                if counter > 0 and counter % 1000 == 0:
+                    logging.info("%s funds processed...", counter)
+                counter += 1
+    
     def run(self):
         fund_list = self._crawl()
         self._output(fund_list)
-        logging.info("List of available funds written to %s", self.output_fp)
 
 
 def main(config_file):
@@ -59,10 +57,12 @@ def main(config_file):
     else:
         logging.basicConfig(level=logging.INFO,
                             format="%(asctime)s - %(levelname)s - %(message)s")
-    fund_univ_url = conf.get("url")
+    fund_univ_url = conf.get("source").get("univ_url")
     fund_univ_file = conf.get("output").get("fund_univ")
+    logging.info("Acquire fund data from %s", fund_univ_url)
     fund_univ_worker = FundUnivWorker(fund_univ_url, fund_univ_file)
     fund_univ_worker.run()
+    logging.info("List of available funds written to %s", fund_univ_file)
 
 
 if __name__ == "__main__":
