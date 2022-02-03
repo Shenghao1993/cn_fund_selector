25a8eacaf46b55c4a532d6ae01346dd45747a034
Shenghao1993 <wangshenghao1993@gmail.com>
2022-02-03 18:59:01 +0800

    Backup the previous work

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # cn_fund_selector
+
 Compare historical performance of CN funds and give recommendation
+
+streamlit run src/screener/app.py
---
+++ src/config/__init__.py
@@ -0,0 +1,6 @@
+import os
+
+DATE_FMT = "%Y-%m-%d"
+PROJECT_ROOT = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), os.pardir, os.pardir)
+)
